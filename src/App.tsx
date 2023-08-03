6359d3c1dc3c80338c8ff7bae097f94e577c2180
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-08-03 13:47:33 -0400

    create header and intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
 import "./App.css";
 import Header from "./components/Header";
+import Intro from "./components/Intro";
 
 function App() {
   return (
     <div>
       <Header />
+      <main>
+        <Intro />
+      </main>
     </div>
   );
 }
---
+++ src/components/Intro.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Intro = () => {
+  return (
+    <section className="intro">
+      <div>
+        <h1>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam,
+          maiores!
+        </h1>
+
+        <div className="socials">
+          <div>logo</div>
+          <div>logo</div>
+          <div>logo</div>
+          <div>logo</div>
+          <div>logo</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
