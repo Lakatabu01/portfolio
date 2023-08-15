d74102c28c047fae20b920f1d5b71a2d9949563a
olannrewaju <Bamgbadeolanrewaju@gmail.com>
2023-08-15 05:04:55 -0400

    create drop-down

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,38 +3,92 @@ import "../App.css";
 import Icon from "@mdi/react";
 import { mdiStar } from "@mdi/js";
 import { mdiMenu } from "@mdi/js";
+import { mdiClose } from "@mdi/js";
+
+import { useState, useEffect, useRef } from "react";
 
 const Header: React.FC = () => {
+  const [dropDown, setDropDown] = useState<boolean>(false);
+  const targetElementRef = useRef<HTMLDivElement | null>(null);
+
+  const handleDropDown = () => {
+    setDropDown(!dropDown);
+  };
+
+  //cancel header drop down if any part of the widow is clicked
+  //except header
+  const handleOutsideClick = (event: MouseEvent) => {
+    if (
+      targetElementRef.current &&
+      !targetElementRef.current.contains(event.target as Node)
+    ) {
+      setDropDown(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("click", handleOutsideClick);
+  });
+
   return (
-    <header>
-      <div className="star">
-        <Icon path={mdiStar} size={1} />
+    <header ref={targetElementRef} className={`${dropDown ? "show" : ""}`}>
+      <div className="header">
+        <div className="star">
+          <Icon path={mdiStar} size={1} />
+        </div>
+
+        <nav className="list-nav">
+          <ul>
+            <li>
+              {" "}
+              <button className="nav-button">About</button>{" "}
+            </li>
+            <li>
+              {" "}
+              <button className="nav-button">Contact</button>
+            </li>
+            <li>
+              {" "}
+              <button className="nav-button">Resume</button>{" "}
+            </li>
+            <li>
+              {" "}
+              <button className="nav-button">Projects</button>{" "}
+            </li>
+          </ul>
+        </nav>
+
+        <nav className="menu-nav">
+          <button onClick={handleDropDown} className="menu-btn">
+            <Icon
+              path={!dropDown ? mdiMenu : mdiClose}
+              size={1}
+              color="white"
+            />
+          </button>
+        </nav>
       </div>
 
-      <nav className="list-nav">
+      <div className={`drop-down ${dropDown ? "show" : ""}`}>
         <ul>
-          <li>
-            {" "}
-            <button className="nav-button">About</button>{" "}
+          <li className="drop-list">
+            <button>About</button>
           </li>
-          <li>
-            {" "}
-            <button className="nav-button">Contact</button>
+          <li className="drop-list">
+            <button>Contact</button>
           </li>
-          <li>
-            {" "}
-            <button className="nav-button">Resume</button>{" "}
+          <li className="drop-list">
+            <button>Resume</button>
+          </li>
+          <li className="drop-list">
+            <button>Projects</button>
           </li>
         </ul>
-      </nav>
-
-      <nav className="menu-nav">
-        <button className="menu-btn">
-          <Icon path={mdiMenu} size={1} color="white" />
-        </button>
-      </nav>
+      </div>
     </header>
   );
 };
 
 export default Header;
+
+//`drop-down ${dropDown ? "show" : ""}`
