84bb6f3dd6f409fa31bc5818156c4f667aa8efd1
Olanrewaju <lanrebamgbade@gmail.com>
2023-09-06 10:09:15 -0700

    change the import of defineconfig from vite to vitest/config

--- tests/intro.test.tsx
+++ tests/intro.test.tsx
@@ -1,4 +1,4 @@
-import { getByTestId, render, screen } from "@testing-library/react";
+import { getByTestId, render } from "@testing-library/react";
 import { describe, it, expect } from "vitest";
 import React from "react";
 import Intro from "../src/components/Intro";
